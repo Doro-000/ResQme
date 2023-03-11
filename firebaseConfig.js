ed109a4597a4f845ed0885f155a2b99159357e65
Doro-000 <hizkyastekle@gmail.com>
2023-03-11 13:45:34 +0100

    Initial Commit

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzxGlqQSYieBc5n2jTzELulBFvqEWAHKc",
+  authDomain: "resqme-4365d.firebaseapp.com",
+  projectId: "resqme-4365d",
+  storageBucket: "resqme-4365d.appspot.com",
+  messagingSenderId: "778284084529",
+  appId: "1:778284084529:web:7ea81997f3f2de4f949410",
+  measurementId: "G-WZFB9VZQ93",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
